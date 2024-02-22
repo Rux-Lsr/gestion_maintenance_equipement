912ce6916330d47014e235c054c224fc59efa2cc
unknown <njaanselme@gmail.com>
2024-02-22 14:28:22 +0100

    ajout du repertoire de model qui contient toutes les classes

---
+++ Gestion_Materiel_Informatique/src/model/Notification.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Notification {
+    
+    private int id_notification;
+    private int id_maintenance;
+    private String description_notification;
+    private Date date_notification = new Date();
+
+    
+}
